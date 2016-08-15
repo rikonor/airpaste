8ce3451a5dcfa6abae00e58cfc4394b8beb00fd1
Or Rikon <rikonor@gmail.com>
2016-08-14 20:12:54 -0400

    pipe stdin

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+// isPiped checks if something is being piped to stdin
+func isPiped() bool {
+	fs, err := os.Stdin.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return (fs.Mode() & os.ModeCharDevice) == 0
+}
 
 func main() {
-	fmt.Println("Hello")
+	// Check if something is being piped in
+	if !isPiped() {
+		fmt.Println("Nothing is being piped in")
+		os.Exit(0)
+	}
+
+	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
+
+	_, err := r.WriteTo(w)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Flush()
 }
